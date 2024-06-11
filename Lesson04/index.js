b0902ab9151161b8400d012c7e4c198de0488682
amolpalwe2002 <amolpalwe2002@gmail.com>
2024-06-11 14:13:02 +0530

    add lesson04

--- Lesson04/index.js
+++ Lesson04/index.js
@@ -2,33 +2,49 @@ const text = document.getElementById("main").innerText;
 
 const splitText = text.split("");
 let temp = "";
-splitText.forEach(element => {
-    temp += `<span>${element}</span>`
+splitText.forEach((element) => {
+  temp += `<span>${element}</span>`;
 });
 
 document.getElementById("main").innerHTML = temp;
 
 gsap.from("#main span", {
-    y:10,
-    opacity:0,
-    stagger: 0.2,
-    ease: 'elastic',
+  y: 10,
+  opacity: 0,
+  yoyo: true,
+  repeat: -1,
+  stagger: 0.2,
+  ease: "elastic",
 
-    
-    // stagger:{
-    //     each: 0.5,
-    //     from: 'center',
-    //     grid: "auto",
-    //     ease: 'bounce',
-    //     // repeat: -1
-    // },
-    duration:1,
-})
+  // stagger:{
+  //     each: 0.5,
+  //     from: 'center',
+  //     grid: "auto",
+  //     ease: 'bounce',
+  //     // repeat: -1
+  // },
+  duration: 1,
+});
 
+gsap.to(".box", {
+  duration: 1,
+  scale: 0.1,
+  y: 60,
+  yoyo: true,
+  repeat: -1,
+  ease: "power1.inOut",
+  stagger: {
+    amount: 1.5,
+    grid: "auto",
+    axis: "x",
+    ease: "power1.inOut",
+    from: "center",
+  },
+});
 
-gsap.from("h1",{
-    opacity:0,
-    y:5,
-    duration:0.5,
-    delay:0.2
-})
+gsap.from("h1", {
+  opacity: 0,
+  y: 5,
+  duration: 0.5,
+  delay: 0.2,
+});
